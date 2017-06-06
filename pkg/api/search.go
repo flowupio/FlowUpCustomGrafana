c27047bffd99c0a648d85ab91d122fbd131cbf8c
Sergio Gutierrez <sergio@gokarumi.com>
2017-06-06 17:21:24 +0200

    Remove home dashboard from dashboard search to prevent repetitions

--- pkg/api/search.go
+++ pkg/api/search.go
@@ -6,6 +6,7 @@ import (
 	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/metrics"
 	"github.com/grafana/grafana/pkg/middleware"
+	m "github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/services/search"
 )
 
@@ -43,6 +44,22 @@ func Search(c *middleware.Context) {
 		return
 	}
 
+	prefsQuery := m.GetPreferencesWithDefaultsQuery{OrgId: c.OrgId, UserId: c.UserId}
+	if err := bus.Dispatch(&prefsQuery); err != nil {
+		c.JsonApiErr(500, "Failed to get preferences", err)
+	}
+
+	dashboards := search.HitList{}
+	if prefsQuery.Result.HomeDashboardId != 0 {
+		for _, hit := range searchQuery.Result {
+			if prefsQuery.Result.HomeDashboardId != hit.Id {
+				dashboards = append(dashboards, hit)
+			}
+		}
+	} else {
+		dashboards = searchQuery.Result
+	}
+
 	c.TimeRequest(metrics.M_Api_Dashboard_Search)
-	c.JSON(200, searchQuery.Result)
+	c.JSON(200, dashboards)
 }
